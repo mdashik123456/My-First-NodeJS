44c8825149ae8866f003e59d1e9e1b3b8d70a67c
mdashik123456 <mdashiik1404@gmail.com>
2023-10-05 23:06:27 +0600

    update

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res)=>{
-    res.send("This is get response");
+    res.send("This is get response") ;
 });
 app.post("/", (req, res)=>{
     res.send("This is post response");
