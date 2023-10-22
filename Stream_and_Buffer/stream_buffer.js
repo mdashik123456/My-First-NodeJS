33443eae638f304e25cf344471dcb3dd727f3bd5
mdashik123456 <mdashiik1404@gmail.com>
2023-10-23 02:15:48 +0600

    buffer and stream

--- Stream_and_Buffer/stream_buffer.js
+++ Stream_and_Buffer/stream_buffer.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 
 const ourReadStream = fs.createReadStream(`${__dirname}/data.txt`);
 
+// this file content is printed as Buffer. Thats why we use .toString() function to convert buffer to string.
 ourReadStream.on('data', (chunk) => {
     console.log(chunk.toString());
 });
