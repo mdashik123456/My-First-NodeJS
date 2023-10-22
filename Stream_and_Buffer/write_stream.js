23d51847a688d5385fb972835198368b1b43f9ac
mdashik123456 <mdashiik1404@gmail.com>
2023-10-23 02:50:24 +0600

    update

---
+++ Stream_and_Buffer/write_stream.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+
+const ourReadStream = fs.createReadStream(`${__dirname}/data.txt`);
+const ourWriteStream = fs.createWriteStream(`${__dirname}/output.txt`);
+
+// ourReadStream.on("data", (chunk) => {
+//     ourWriteStream.write(chunk);
+// });
+
+// this two methoods are same 
+
+ourReadStream.pipe(ourWriteStream);
